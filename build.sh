1685c3295f6c4192cd1f38a26853cacabd3f52f3
Quan MT <info@brodev.com>
2014-07-05 10:01:42 +0700

    Remove unused files from build

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 rm -rf build
+rm -f facebookless.zip
 mkdir build
-rsync -av --exclude='build' --exclude='.idea' --exclude='.git*' --exclude='build.sh' ./ ./build/
+rsync -av --exclude='build' --exclude='.idea' --exclude='.git*' --exclude='build.sh' --exclude='facebookless.zip' ./ ./build/
 
 cp ./key.pem ./build/key.pem
 zip -r facebookless.zip build
